8fa87332bd246f15d85a6b37a5ee2db978759f94
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-11-20 06:09:34 +0530

    Sort section updated

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -6,7 +6,7 @@ const FilterContext = createContext();
 const initialState = {
     filter_product: [],
     all_product: [],
-    grid_view: false,
+    grid_view: true,
 }
 
 export const FilterContextProvider = ({ children }) => {
@@ -18,12 +18,15 @@ export const FilterContextProvider = ({ children }) => {
     const setGridView = () => {
         return dispatch({ type: "SET_GRID_VIEW" })
     }
+    const setListView = () => {
+        return dispatch({ type: "SET_LIST_VIEW" })
+    }
 
     useEffect(() => {
         dispatch({ type: "LOAD_FILTER_DATA", payload: products })
     }, [products]);
     return (
-        <FilterContext.Provider value={{ ...state, setGridView }}>
+        <FilterContext.Provider value={{ ...state, setGridView , setListView}}>
             {children}
         </FilterContext.Provider>
     );
